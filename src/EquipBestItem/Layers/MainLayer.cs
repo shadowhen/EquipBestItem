d243e32c5ad8f3988aaf9dee6b28ff59de125fa9
Henry <shadowhen89@gmail.com>
2020-11-30 13:22:32 -0600

    buttons will now show if upgrade exists when pressing inventory key

--- src/EquipBestItem/Layers/MainLayer.cs
+++ src/EquipBestItem/Layers/MainLayer.cs
@@ -1,11 +1,13 @@
-﻿using TaleWorlds.Core;
-using TaleWorlds.Engine.GauntletUI;
+﻿using TaleWorlds.Engine.GauntletUI;
 
 namespace EquipBestItem
 {
     class MainLayer : GauntletLayer
     {
-        MainViewModel _viewModel;
+        private MainViewModel _viewModel;
+
+        private bool _firstRefresh = false;
+        private bool _leftMouseButtonWasReleased = false;
 
         public MainLayer(int localOrder, string categoryId = "GauntletLayer") : base(localOrder, categoryId)
         {
@@ -13,11 +15,18 @@ namespace EquipBestItem
             this.LoadMovie("EBIInventory", this._viewModel);
         }
 
-        bool _leftMouseButtonWasReleased = false;
-
         protected override void OnLateUpdate(float dt)
         {
             base.OnLateUpdate(dt);
+
+            // Since we are refreshing for the first time, we need to check for first refresh
+            // so we are not refreshing every single late update
+            if (!_firstRefresh)
+            {
+                _firstRefresh = true;
+                _viewModel.RefreshValues();
+                return;
+            }
 
             if (TaleWorlds.InputSystem.Input.IsKeyReleased(TaleWorlds.InputSystem.InputKey.LeftMouseButton) && !_leftMouseButtonWasReleased)
             {
